b451f30ccbd299ea18adf2fd9fc340ae127c9846
siva sanker <sivsanker744@gmail.com>
2025-12-11 18:50:59 +0530

    setup trial host via render

--- expense/settings.py
+++ expense/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xv&e&i^0!@$(gi_$ez10$r%&6gthgoza2azxwriq#ta7=1kej8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
